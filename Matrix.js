65b1848458b741664f58e6d44ad5bf07d3e4ed08
Yubraj Sharma <therealyubraj@gmail.com>
2020-02-27 21:14:11 +0545

    Add files via upload

---
+++ Matrix.js
@@ -0,0 +1,155 @@
+function sigmoid(x){
+  return 1/(1 + Math.exp(-x));
+}
+function DSigmoid(y){
+  return y * (1 - y);
+}
+class Matrix {
+  constructor(r_, c_) {
+    this.mat = [];
+    this.rows = r_;
+    this.cols = c_;
+    for (let i = 0; i < this.rows; i++) {
+      this.mat[i] = [];
+      for (let j = 0; j < this.cols; j++) {
+        this.mat[i][j] = 0;
+      }
+    }
+  }
+
+  static matrixSum(a, b) {
+    let tmp = new Matrix(a.rows, a.cols);
+    for (let i = 0; i < a.rows; i++) {
+      for (let j = 0; j < a.cols; j++) {
+        tmp.mat[i][j] = a.mat[i][j] + b.mat[i][j];
+      }
+    }
+    return tmp;
+  }
+
+  static matrixSub(a, b) {
+    let tmp = new Matrix(a.rows, a.cols);
+    for (let i = 0; i < a.rows; i++) {
+      for (let j = 0; j < a.cols; j++) {
+        tmp.mat[i][j] = a.mat[i][j] - b.mat[i][j];
+      }
+    }
+    return tmp;
+  }
+
+  static hadamardMult(a, b) {
+    let tmp = new Matrix(a.rows, a.cols);
+    for (let i = 0; i < a.rows; i++) {
+      for (let j = 0; j < a.cols; j++) {
+        tmp.mat[i][j] = a.mat[i][j] * b.mat[i][j];
+      }
+    }
+    return tmp;
+  }
+
+  static matrixMult(a, b) {
+    if (a.cols == b.rows) {
+      let tmp = new Matrix(a.rows, b.cols);
+      for (let i = 0; i < tmp.rows; i++) {
+        for (let j = 0; j < tmp.cols; j++) {
+          let sum = 0;
+          for (let k = 0; k < b.rows; k++) {
+            sum += a.mat[i][k] * b.mat[k][j];
+          }
+          tmp.mat[i][j] = sum;
+        }
+      }
+      return tmp;
+    } else {
+      console.error("Cant Multiply!!!");
+    }
+  }
+
+  static transpose(a) {
+    let tmp = new Matrix(a.cols, a.rows);
+    for (let i = 0; i < tmp.rows; i++) {
+      for (let j = 0; j < tmp.cols; j++) {
+        tmp.mat[i][j] = a.mat[j][i];
+      }
+    }
+    return tmp;
+  }
+
+  static toArray(m) {
+    let res = [];
+    let count = 0;
+    for (let i = 0; i < m.rows; i++) {
+      for (let j = 0; j < m.cols; j++) {
+        res[count] = m.mat[i][j];
+        count++;
+      }
+    }
+    return res;
+  }
+
+  static fromArray(arr) {
+    let res = new Matrix(arr.length, 1);
+    let count = 0;
+    for (let i = 0; i < res.rows; i++) {
+      for (let j = 0; j < res.cols; j++) {
+        res.mat[i][j] = arr[count];
+        count++;
+      }
+    }
+    return res;
+  }
+
+  static copyMatrix(a) {
+    let res = new Matrix(a.rows, a.cols);
+    for (let i = 0; i < a.rows; i++) {
+      for (let j = 0; j < a.cols; j++) {
+        res.mat[i][j] = a.mat[i][j];
+      }
+    }
+    return res;
+  }
+
+  static matrixMap(a, fn) {
+    let tmp = new Matrix(a.rows, a.cols);
+    tmp = Matrix.copyMatrix(a);
+    for (let i = 0; i < a.rows; i++) {
+      for (let j = 0; j < a.cols; j++) {
+        tmp.mat[i][j] = fn(tmp.mat[i][j]);
+      }
+    }
+    return tmp;
+  }
+
+  randomize() {
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        this.mat[i][j] = Math.random() * 2 - 1;
+      }
+    }
+  }
+
+  scalarAdd(n) {
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        this.mat[i][j] += n;
+      }
+    }
+  }
+
+  scalarMult(n) {
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        this.mat[i][j] *= n;
+      }
+    }
+  }
+  matrixSum(b) {
+    let tmp = new Matrix(this.rows, this.cols);
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        this.mat[i][j] += b.mat[i][j];
+      }
+    }
+    return tmp;
+  }
+}
---
+++ NeuralNetwork.js
@@ -0,0 +1,93 @@
+class NN {
+  constructor(inputs, hiddens, outputs, noH) {
+    this.total = noH + 2;
+    this.weights = [];
+    this.biases = [];
+    this.lr = 0.1;
+
+    this.weights[0] = new Matrix(hiddens, inputs);
+    this.weights[this.total - 2] = new Matrix(outputs, hiddens);
+
+    this.biases[0] = new Matrix(hiddens, 1);
+    this.biases[this.total - 2] = new Matrix(outputs, 1);
+
+    for (let i = 1; i < this.total-2; i++) {
+      this.weights[i] = new Matrix(hiddens, hiddens);
+      this.biases[i] = new Matrix(hiddens, 1);
+    }
+
+    for (let i = 0; i < this.total-1; i++) {
+      this.weights[i].randomize();
+      this.biases[i].randomize();
+    }
+  }
+
+  predict(ip) {
+    let inputs = Matrix.fromArray(ip);
+
+    let weightedSums = [];
+
+    weightedSums[0] = Matrix.copyMatrix(inputs);
+
+    for (let i = 1; i < this.total; i++) {
+      weightedSums[i] = Matrix.matrixMult(this.weights[i - 1], weightedSums[i - 1]);
+      weightedSums[i] = Matrix.matrixSum(weightedSums[i], this.biases[i - 1]);
+      weightedSums[i] = Matrix.matrixMap(weightedSums[i], sigmoid);
+    }
+
+    let op = Matrix.toArray(weightedSums[this.total - 1]);
+
+    return op;
+  }
+
+  train(ip, t){
+    let inputs = Matrix.fromArray(ip);
+
+    let weightedSums = [];
+
+    weightedSums[0] = Matrix.copyMatrix(inputs);
+
+    for (let i = 1; i < this.total; i++) {
+      weightedSums[i] = Matrix.matrixMult(this.weights[i - 1], weightedSums[i - 1]);
+      weightedSums[i] = Matrix.matrixSum(weightedSums[i], this.biases[i - 1]);
+      weightedSums[i] = Matrix.matrixMap(weightedSums[i], sigmoid);
+    }
+
+    let output = Matrix.copyMatrix(weightedSums[this.total - 1]);
+
+    let target = Matrix.fromArray(t);
+
+    let errors = [];
+
+    errors[this.total - 2] = Matrix.matrixSub(target, output);
+
+    for(let i = this.total - 3;i >= 0;i--){
+      let trans = Matrix.transpose(this.weights[i+1]);
+      errors[i] = Matrix.matrixMult(trans, errors[i+1]);
+    }
+
+    let DSigWeightedSums = [];
+
+    for(let  i = 0; i < weightedSums.length;i++){
+      DSigWeightedSums[i] = Matrix.matrixMap(weightedSums[i], DSigmoid);
+    }
+
+    let gradients = [];
+    let weights_deltas = [];
+
+    for(let i = 0;i < this.total - 1;i++){
+      gradients[i] = Matrix.hadamardMult(DSigWeightedSums[i+1], errors[i]);
+      gradients[i].scalarMult(this.lr);
+    }
+
+    for(let i = 0;i < this.total - 1;i++){
+      let trans = Matrix.transpose(weightedSums[i]);
+      weights_deltas[i] = Matrix.matrixMult(gradients[i], trans);
+    }
+
+    for(let i = 0;i < this.total - 1;i++){
+      this.weights[i].matrixSum(weights_deltas[i]);
+      this.biases[i].matrixSum(gradients[i]);
+    }
+  }
+}
